44359eff3c85dff7c0cdca64847dece541336641
ARCJ137442 <61109168+ARCJ137442@users.noreply.github.com>
2024-05-14 02:45:36 +0800

    revert: :wrench: 将构建脚本所构建的「版本号」回退到3.1.2，即「312」

--- build.bat
+++ build.bat
@@ -14,25 +14,25 @@ javac -d classes @sources.txt -Xstdout compile.log
 @REM ==后续其它构建代码==
 @REM 1 构建NARS代码（classes）到jar文件（GUI）
 echo Main-Class: nars.gui.main > manifest.txt
-jar -cvfm opennars-31x-gui.jar manifest.txt -C classes .
+jar -cvfm opennars-312-gui.jar manifest.txt -C classes .
 DEL manifest.txt
 
 @REM 2 构建NARS代码（classes）到jar文件（Shell）
 echo Main-Class: nars.main.Shell > manifest.txt
-jar -cvfm opennars-31x-shell.jar manifest.txt -C classes .
+jar -cvfm opennars-312-shell.jar manifest.txt -C classes .
 DEL manifest.txt
 
 @REM 2 构建NARS代码（classes）到jar文件（Batch）
 echo Main-Class: nars.main.NARSBatch > manifest.txt
-jar -cvfm opennars-31x-batch.jar manifest.txt -C classes .
+jar -cvfm opennars-312-batch.jar manifest.txt -C classes .
 DEL manifest.txt
 
 @REM 后续信息
 echo You can now launch:
-echo java -jar opennars-31x-gui.jar
+echo java -jar opennars-312-gui.jar
 echo or
-echo java -jar opennars-31x-gui.jar nars-dist/Examples/Example-NAL1-edited.txt --silence 90
+echo java -jar opennars-312-gui.jar nars-dist/Examples/Example-NAL1-edited.txt --silence 90
 echo or
-echo java -jar opennars-31x-shell.jar
+echo java -jar opennars-312-shell.jar
 echo or
-echo java -jar opennars-31x-batch.jar nars-dist/Examples/Example-NAL1-edited.txt
+echo java -jar opennars-312-batch.jar nars-dist/Examples/Example-NAL1-edited.txt
